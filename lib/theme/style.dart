6ad449bbfe3c26d455a4c1e9df09386c70c4ab55
Vinh-Thuyen <ntvthuyen@gmail.com>
2020-04-04 13:40:36 +0700

    Modified theme/style.dart

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -4,6 +4,11 @@ final TextStyle tstitle = const TextStyle(
 		fontSize: 36,
 		fontFamily: 'Noto Sans'
 		);
+final TextStyle tsraisedbutton = const TextStyle(
+		fontSize: 18,
+		fontFamily: 'Noto Sans',
+		color: Colors.white
+		);
 final TextTheme textTheme = const TextTheme(
 		button: const TextStyle(color: Colors.white)	
 );
